e7718057def304fa2c9d99c979447765b46b4186
Lucas de Albuquerque Miranda <lucas.a.miranda.dev@gmail.com>
2025-05-22 17:24:29 -0300

    Primeiro Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+import frame.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        new Principal();
+    }
+}
---
+++ src/frame/Principal.java
@@ -0,0 +1,50 @@
+package frame;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Principal extends JFrame {
+
+    private static final int LARGURA_TELA = 500;
+    private static final int ALTURA_TELA = 500;
+
+    public Principal(){
+
+
+
+        JFrame tela = new JFrame();
+
+        tela.setTitle("TELA");
+        tela.setSize(LARGURA_TELA,ALTURA_TELA);
+        tela.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        tela.setResizable(false);
+        tela.setLocationRelativeTo(null);
+
+        ImageIcon icon = new ImageIcon("images/icon.png");
+        tela.setIconImage(icon.getImage());
+
+        JLabel fundo = new JLabel(new ImageIcon("images/fundo.png"));
+        fundo.setSize(1000,1000);
+        tela.setContentPane(fundo);
+
+        JButton botao = new JButton("Jogar");
+        botao.setSize(LARGURA_TELA/5, ALTURA_TELA/15);
+
+        botao.setBounds((LARGURA_TELA-botao.getWidth())/2,(ALTURA_TELA-botao.getHeight())/2,botao.getWidth(), botao.getHeight());
+
+        try {
+            Font font = Font.createFont(Font.TRUETYPE_FONT, new File("src/fontes/PressStart2P-Regular.ttf"))
+                    .deriveFont(Font.PLAIN, 24f);
+            botao.setFont(font);
+        }
+        catch (FontFormatException | IOException e) {
+            System.out.println("ERROR: " + e.getMessage());
+            // Se der erro, usa uma fonte padrão
+            botao.setFont(new Font("SansSerif", Font.PLAIN, 24));
+        }
+        tela.add(botao);
+        tela.setVisible(true);
+
+    }
+}
